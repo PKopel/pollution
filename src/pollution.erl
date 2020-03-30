d7ab6faaa459ba05cb62d0c3c278cc7edcc42797
PKopel <pawel.kopel2@gmail.com>
2020-03-30 18:35:11 +0200

    working basic functions

--- src/pollution.erl
+++ src/pollution.erl
@@ -8,133 +8,98 @@
 %%%-------------------------------------------------------------------
 -module(pollution).
 -author("pkopel").
+-record(station, {name, coords, measurements = []}).
 
 %% API
 -export([createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4, getStationMean/3, getDailyMean/3]).
 
-getName({Name, _, _}) ->
-  Name.
-
-getCoords({_, Coords, _}) ->
-  Coords.
-
-newStation(Name, Coords) ->
-  {{Name, Coords, []}, {empty, empty}, {empty, empty}}.
-
-searchMonitor({Current, NameChildren, {Smaller, Larger}}, {X, Y}, Func) ->
-  CurrentCoords = getCoords(Current),
-  if CurrentCoords > {X, Y} ->
-    case searchMonitor(Smaller, {X, Y}, Func) of
-      {D, NC, CC} -> {Current, NameChildren, {{D, NC, CC}, Larger}};
-      Other -> Other
-    end;
-    CurrentCoords < {X, Y} ->
-      case searchMonitor(Larger, {X, Y}, Func) of
-        {D, NC, CC} -> {Current, NameChildren, {Smaller, {D, NC, CC}}};
-        Other -> Other
-      end;
-    true -> Func({Current, NameChildren, {Smaller, Larger}})
+searchMonitor([Current | T], {X, Y}, Func) ->
+  if Current#station.coords == {X, Y} -> Func([Current | T]);
+    true -> Result = searchMonitor(T, {X, Y}, Func),
+      if is_list(Result) -> [Current | Result];
+        true -> Result
+      end
   end;
-searchMonitor({Current, {Smaller, Larger}, CoordsChildren}, Name, Func) ->
-  CurrentName = getName(Current),
-  if CurrentName > Name ->
-    case searchMonitor(Smaller, Name, Func) of
-      {D, NC, CC} -> {Current, {{D, NC, CC}, Larger}, CoordsChildren};
-      Other -> Other
-    end;
-    CurrentName < Name ->
-      case searchMonitor(Larger, Name, Func) of
-        {D, NC, CC} -> {Current, {Smaller, {D, NC, CC}}, CoordsChildren};
-        Other -> Other
-      end;
-    true -> Func({Current, {Smaller, Larger}, CoordsChildren})
+searchMonitor([Current | T], Name, Func) ->
+  if Current#station.name == Name -> Func([Current | T]);
+    true -> Result = searchMonitor(T, Name, Func),
+      if is_list(Result) -> [Current | Result];
+        true -> Result
+      end
   end;
 searchMonitor(Monitor, _, Func) ->
   Func(Monitor).
 
-walkMonitor({Current, {Smaller, Larger}, _}, Func, Acc) ->
-  SmallerAcc = walkMonitor(Smaller, Func, Acc),
-  LargerAcc = walkMonitor(Larger, Func, SmallerAcc),
-  Func(Current, LargerAcc);
-walkMonitor(Monitor, Func, Acc) ->
-  Func(Monitor, Acc).
-
-
-
 createMonitor() ->
-  empty.
+  [].
 
-addStation(empty, Name, Coords) ->
-  newStation(Name, Coords);
 addStation(Monitor, Name, Coords) ->
-  NewStation = newStation(Name, Coords),
-  AddStation = fun(empty) -> NewStation;
-    ({N, C, _}) ->
-      if N == Name -> {error, name_already_used};
-        C == Coords -> {error, coords_already_used};
+  AddStation = fun([]) -> [#station{name = Name, coords = Coords}];
+    ([S | _]) ->
+      if S#station.name == Name -> {error, name_already_used};
+        S#station.coords == Coords -> {error, coords_already_used};
         true -> {error, wrong_arguments}
       end;
     (_) -> {error, wrong_arguments} end,
-  NewMonitor = searchMonitor(Monitor, Name, AddStation),
-  searchMonitor(NewMonitor, Coords, AddStation).
+  searchMonitor(Monitor, Coords, AddStation).
+
+filter(Date, Type) -> fun({D, T, _}) when D == Date, T == Type -> true;(_) -> false end.
 
 addValue(Monitor, Station, Date, Type, Value) ->
-  AddValue = fun({{N, Coords, List}, NameChildren, CoordsChildren}) ->
-    case lists:any(fun({D, T, _}) when D == Date, T == Type -> true;
-      (_) -> false
-                   end, List) of
+  AddValue = fun([S | T]) ->
+    case lists:any(filter(Date, Type), S#station.measurements) of
       true -> {error, measurement_already_recorded};
-      false -> {{N, Coords, [{Date, Type, Value} | List]}, NameChildren, CoordsChildren}
+      false -> OldList = S#station.measurements,
+        [S#station{measurements = [{Date, Type, Value} | OldList]} | T]
     end;
-    (_) -> {error, no_such_station} end,
+    ([]) -> {error, no_such_station} end,
   searchMonitor(Monitor, Station, AddValue).
 
 removeValue(Monitor, Station, Date, Type) ->
-  RemoveValue = fun({{N, Coords, List}, NameChildren, CoordsChildren}) ->
-    Filter = fun({D, T, _}) when D == Date, T == Type -> true;
-      (_) -> false
-             end,
-    case lists:any(Filter, List) of
-      true -> {{N, Coords, lists:filter(fun(R) -> not Filter(R) end, List)}, NameChildren, CoordsChildren};
+  Filter = filter(Date, Type),
+  RemoveValue = fun([S | T]) ->
+    case lists:any(Filter, S#station.measurements) of
+      true -> OldList = S#station.measurements,
+        [S#station{measurements = lists:filter(fun(R) -> not Filter(R) end, OldList)} | T];
       false -> {error, no_such_record}
     end;
-    (_) -> {error, no_such_station} end,
+    ([]) -> {error, no_such_station} end,
   searchMonitor(Monitor, Station, RemoveValue).
 
 getOneValue(Monitor, Station, Date, Type) ->
-  SearchStation = fun({{N, C, List}, _, _}) when C == Station; N == Station -> List;
-    (_) -> {error, no_such_station}
-                  end,
-  List = searchMonitor(Monitor, Station, SearchStation),
-  case lists:filter(fun({D, T, _}) when D == Date, T == Type -> true; (_) -> false end, List) of
-    [{Date, Type, Value}] -> Value;
-    [] -> {error, no_such_measurement}
-  end.
+  GetValue = fun([S | _]) ->
+    case lists:filter(filter(Date, Type), S#station.measurements) of
+      [{Date, Type, Value}] -> Value;
+      [] -> {error, no_such_measurement}
+    end;
+    ([]) -> {error, no_such_station}
+             end,
+  searchMonitor(Monitor, Station, GetValue).
 
 getStationMean(Monitor, Station, Type) ->
-  TypeMean = fun({{_, _, List}, _, _}) ->
+  TypeMean = fun([S | _]) ->
     case lists:foldl(fun({_, T, V}, {Sum, N}) ->
       if T == Type -> {Sum + V, N + 1};
         true -> {Sum, N}
       end
-                     end, {0, 0}, List) of
+                     end, {0, 0}, S#station.measurements) of
       {0, 0} -> {error, no_such_record};
       {Value, Number} -> Value / Number
     end;
-    (_) -> {error, no_such_station} end,
+    ([]) -> {error, no_such_station} end,
   searchMonitor(Monitor, Station, TypeMean).
 
 getDailyMean(Monitor, Type, {Year, Month, Day}) ->
-  DailyMean = fun({_, _, List}, {SumAcc, NumAcc}) ->
+  DailyMean = fun(S, {SumAcc, NumAcc}) ->
     {Value, Number} = lists:foldl(fun({{{Y, M, D}, _}, T, V}, {Sum, N}) ->
       if T == Type, Y == Year, M == Month, D == Day -> {Sum + V, N + 1};
         true -> {Sum, N}
       end
-                                  end, {0, 0}, List),
+                                  end, {0, 0}, S#station.measurements),
     {SumAcc + Value, NumAcc + Number};
     (empty, Acc) -> Acc;
     (_, _) -> {error, wrong_arguments} end,
-  case walkMonitor(Monitor, DailyMean, {0, 0}) of
+  case lists:foldl(DailyMean, {0, 0}, Monitor) of
     {0, 0} -> 0;
     {Sum, Number} -> Sum / Number;
     _ -> {error, wrong_arguments}
